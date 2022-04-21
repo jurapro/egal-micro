3acd00a302b1aa256d0b064939c0873acaa85524
Юрий Грушевский <jurapro@yandex.ru>
2022-04-21 16:22:48 +0700

    fix bugs

---
+++ monolit-service/database/migrations/2022_04_20_181833_create_speakers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpeakersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('speakers', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uid')
+                ->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('avatar')
+                ->nullable();
+            $table->string('video')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('speakers');
+    }
+}
